4d8cd56ee3232ccf0cc6602768df128ef07524d3
Saira-Bibi <sb1235912@gmail.com>
2025-08-23 10:26:03 -0700

    updated index

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 5000;
 
 app.get('/', (req, res) => {
-    res.send('Well come to backend !');
+    res.json({message : "well come to backend"});
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
