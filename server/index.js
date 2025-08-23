64e7a9e68463ac26c853f9d4dbfa160f78276fca
Saira-Bibi <sb1235912@gmail.com>
2025-08-23 11:01:18 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
-import express from 'express';
+
+import cors from 'cors'
+import express, { json, urlencoded } from "express";
 import * as dotenv from 'dotenv'
 dotenv.config()
 const app = express();
 const PORT = process.env.PORT;
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cors({
+    'origin': '*',
+}));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({message : "well come to backend"});
 });
 
