7dd07fc5cb978a25d5239b5c372916ba2cb0ac57
Saira-Bibi <sb1235912@gmail.com>
2025-09-02 01:35:05 -0700

    connect to mongo atlas

--- server/src/index.js
+++ server/src/index.js
@@ -22,20 +22,28 @@ app.listen(PORT, () => console.log(` Server running on port ${PORT}`));*/
 
 
 
-
 import express from "express";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({ path: "./.env" });
 
 const app = express();
+
+// Connect to database
+connectDB();
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
